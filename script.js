96966447c21e02a5324642f9a6295e9a6e27defa
Glantrox <59131023+Izan2020@users.noreply.github.com>
2023-06-13 20:43:02 +0700

    lagi ah enak

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-window.addEventListener('scroll', function(){
-    const header = document.querySelector('.navbar');
-    const hamburger = document.querySelector(".hamburger");
+const headerTwo = document.querySelector('.navigations-navbar');
+const header = document.querySelector('.navbar');
+const hamburger = document.querySelector(".hamburger");
 
-    header.classList.toggle('sticky', window.scrollY>2);
-    hamburger.classList.toggle('sticky', window.scrollY>2);
+window.addEventListener('scroll', function(){
+    header.classList.toggle('sticky', window.scrollY>0);
+    hamburger.classList.toggle('sticky', window.scrollY>0);
 });
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    headerTwo.classList.toggle("active");
+    });
