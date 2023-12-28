a1f1ac10bb905953a25c34364b0c710486e422a3
Glantrox <59131023+Izan2020@users.noreply.github.com>
2023-12-29 02:50:16 +0700

    latest

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     headerTwo.classList.toggle("active");
     });
+
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.loader')
+
+    document.querySelector(loader).classList.add("loader--hidden");
+    document.querySelector(loader).addEventListener("trasitionend", () => {
+        document.body.removeChild(loader)
+    })
+});
