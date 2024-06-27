5a0237ab156b51e75ccf221fb85d30a2919a6636
Glantrox <59131023+Izan2020@users.noreply.github.com>
2024-06-27 10:41:49 +0700

    yes

--- api/uploads.js
+++ api/uploads.js
@@ -1,10 +1,12 @@
 const multer = require('multer'); 
 const upload = multer({ dest: './uploads/' });
 
-module.exports = upload.single('file'), (req, res) => {
+upload.single('file'), (req, res) => {
     if (req.file) {
         res.status(200).send('Image uploaded successfully: ' + req.file.filename);
     } else {
         res.status(400).send('No image uploaded.');
     }
 };
+
+module.exports = upload;
